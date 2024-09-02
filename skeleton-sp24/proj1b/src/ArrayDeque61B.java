530be36d202de24784f128933d075dcedb00ac55
key4127 <2827092384@qq.com>
2024-09-02 23:08:11 +0800

    proj1b codes without resizing complete

---
+++ skeleton-sp24/proj1b/src/ArrayDeque61B.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
+
+public class ArrayDeque61B<T> implements Deque61B<T> {
+    private T[] items;
+    private int next_first;
+    private int next_last;
+    private int size;
+
+    public ArrayDeque61B() {
+        items = (T[]) new Object[8];
+        next_first = 0;
+        next_last = 0;
+        size = 0;
+    }
+
+    @Override
+    public void addFirst(T x) {
+        items[next_first] = x;
+        next_first = Math.floorMod(next_first - 1, items.length);
+        size += 1;
+    }
+
+    @Override
+    public void addLast(T x) {
+        items[next_last] = x;
+        next_last = Math.floorMod(next_last + 1, items.length);
+        size += 1;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> return_list = new ArrayList<>();
+        for (int i = 1; i <= size; i++) {
+            return_list.add(items[Math.floorMod(next_first + i, items.length)]);
+        }
+        return return_list;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        int remove_index = Math.floorMod(next_first + 1, items.length);
+        T x = get(remove_index);
+        items[remove_index] = null;
+        next_first = remove_index;
+        size -= 1;
+        return x;
+    }
+
+    @Override
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        int remove_index = Math.floorMod(next_last - 1, items.length);
+        T x = get(remove_index);
+        items[remove_index] = null;
+        next_last = remove_index;
+        size -= 1;
+        return x;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return items[Math.floorMod(next_first + index + 1, items.length)];
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return  getRecursive(next_first + 1, index);
+    }
+
+    // if the AList is real big, these may cause an overflow.
+    private T getRecursive(int start, int index) {
+        if (index == 0) {
+            return items[Math.floorMod(start, items.length)];
+        }
+        return getRecursive(start + 1, index - 1);
+    }
+}
