4dbd881a0e1763d424bae802ef87a4e9dfc65249
key4127 <2827092384@qq.com>
2024-09-06 23:03:48 +0800

    lab05 codes complete

--- skeleton-sp24/lab05/src/UnionFind.java
+++ skeleton-sp24/lab05/src/UnionFind.java
@@ -1,29 +1,34 @@
 public class UnionFind {
     // TODO: Instance variables
+    private int[] parent;
 
     /* Creates a UnionFind data structure holding N items. Initially, all
        items are in disjoint sets. */
     public UnionFind(int N) {
         // TODO: YOUR CODE HERE
+        parent = new int[N];
+        for (int i = 0; i < N; i++) {
+            parent[i] = -1;
+        }
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return -parent[find(v)];
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return parent[v];
     }
 
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
         // TODO: YOUR CODE HERE
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
@@ -31,7 +36,14 @@ public class UnionFind {
        function, throw an IllegalArgumentException. */
     public int find(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        if (v < 0 ||  v >= parent.length) {
+            throw new IllegalArgumentException("invalid items");
+        }
+        if (parent[v] < 0) {
+            return v;
+        }
+        parent[v] = find(parent[v]);
+        return parent[v];
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
@@ -41,6 +53,18 @@ public class UnionFind {
        already connected should not change the structure. */
     public void union(int v1, int v2) {
         // TODO: YOUR CODE HERE
+        if (v1 == v2) {
+            return;
+        }
+        int r1 = find(v1);
+        int r2 = find(v2);
+        if (parent[r1] < parent[r2]) {
+            parent[r1] += parent[r2];
+            parent[r2] = r1;
+        } else {
+            parent[r2] += parent[r1];
+            parent[r1] = r2;
+        }
     }
 
 }
